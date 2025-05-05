28d2817b7d2f88aa0a80bfe907df93ba3b485d0d
NhatMinhPhan <nhatminh120767@gmail.com>
2025-05-05 21:09:15 +0700

    Changed variable name for MY_GUILD to make it final

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ intents.message_content = True
 intents.messages = True
 client = discord.Client(intents = intents)
 tree = app_commands.CommandTree(client)
-my_guild = discord.Object(id = os.getenv('MY_SERVER'))
+MY_GUILD = discord.Object(id = os.getenv('MY_SERVER'))
 
 class BotData:
     counting_mode = False
@@ -23,7 +23,7 @@ async def on_ready():
 @tree.command(
     name="hey",
     description="Say hey to the bot!",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def hello(interaction):
     await interaction.response.send_message("Hey!")
@@ -31,7 +31,7 @@ async def hello(interaction):
 @tree.command(
     name="counting_mode",
     description="Check the status of the counting mode (ON/OFF)",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def get_counting_mode(interaction):
     response = "Counting mode is currently "
@@ -42,7 +42,7 @@ async def get_counting_mode(interaction):
 @tree.command(
     name="toggle_counting",
     description="Toggle the status of the counting mode (ON/OFF)",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def toggle_counting_command(interaction):
     response = toggle_counting()
@@ -51,7 +51,7 @@ async def toggle_counting_command(interaction):
 @tree.command(
     name="one_number_per_user",
     description="Check the status of the one_number_per_user setting for counting (ON/OFF)",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def get_one_number_per_user(interaction):
     response = "ONE_NUMBER_PER_USER is currently "
@@ -62,7 +62,7 @@ async def get_one_number_per_user(interaction):
 @tree.command(
     name="toggle_one_number_per_user",
     description="Toggle the status of the one_number_per_user setting for counting (ON/OFF)",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def toggle_one_number_per_user(interaction):
     counting.CountingData.toggle_one_number_per_user()
@@ -74,7 +74,7 @@ async def toggle_one_number_per_user(interaction):
 @tree.command(
     name="counting_lives",
     description="Check the number of lives remaining before the counting game is reset",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def get_counting_lives(interaction):
     response = f"You now have {counting.CountingData.current_lives} lives remaining."
@@ -83,7 +83,7 @@ async def get_counting_lives(interaction):
 @tree.command(
     name="counting_info",
     description="Return all information about the counting game",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def get_counting_info(interaction):
     response = f"CURRENT LIVES: {counting.CountingData.current_lives}\n"
@@ -97,7 +97,7 @@ async def get_counting_info(interaction):
 @tree.command(
     name="counting_high_score",
     description="Check the high score achieved in the counting game",
-    guild=my_guild
+    guild=MY_GUILD
 )
 async def counting_high_score(interaction):
     response = f"COUNTING HIGH SCORE: {counting.CountingData.current_lives}"
@@ -106,7 +106,7 @@ async def counting_high_score(interaction):
 @tree.command(
     name="set_max_lives_to",
     description="Set the number of max counting lives to a certain positive integer",
-    guild=my_guild
+    guild=MY_GUILD
 )
 @app_commands.describe(new_max_lives='The new number of max lives')
 async def set_max_lives_to(interaction, new_max_lives: str):
