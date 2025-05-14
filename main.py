15187ead03e3bbc04f7e39095a1f3248053731a5
NhatMinhPhan <nhatminh120767@gmail.com>
2025-05-14 13:58:38 +0700

    Added formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os # default module
+
 from dotenv import load_dotenv
 from discord import app_commands
 import counting
@@ -28,13 +29,21 @@ async def hello(interaction):
     await interaction.response.send_message("Hey!")
 
 @tree.command(
+    name="hello",
+    description="Say hello to the bot!",
+    guild=MY_GUILD
+)
+async def hello2(interaction):
+    await interaction.response.send_message("Hello!")
+
+@tree.command(
     name="counting_mode",
     description="Check the status of the counting mode (ON/OFF)",
     guild=MY_GUILD
 )
 async def get_counting_mode(interaction):
     response = "Counting mode is currently "
-    response += "ON" if counting.CountingData.counting_mode else "OFF"
+    response += "**ON**" if counting.CountingData.counting_mode else "**OFF**"
     response += "."
     await interaction.response.send_message(response)
 
@@ -48,7 +57,7 @@ async def toggle_counting_command(interaction):
     roles_are_set = await admin_roles_are_set()
     if not roles_are_set:
         response = 'Admin roles for this bot are not set yet.'
-        response += '\nPlease use the command /update_admin_roles to set admin roles for this bot so that admins-only commands work.'
+        response += '\nPlease use the command `/update_admin_roles` to set admin roles for this bot so that admins-only commands work.'
         await interaction.response.send_message(response, ephemeral=True)
         return
     # Check if the user has one of the admin roles set for the bot:
@@ -75,7 +84,7 @@ async def toggle_counting_command(interaction):
 )
 async def get_one_number_per_user(interaction):
     response = "ONE_NUMBER_PER_USER is currently "
-    response += "ON" if counting.CountingData.one_number_per_user else "OFF"
+    response += "**ON**" if counting.CountingData.one_number_per_user else "**OFF**"
     response += "."
     await interaction.response.send_message(response)
 
@@ -89,7 +98,7 @@ async def toggle_one_number_per_user(interaction):
     roles_are_set = await admin_roles_are_set()
     if not roles_are_set:
         response = 'Admin roles for this bot are not set yet.'
-        response += '\nPlease use the command /update_admin_roles to set admin roles for this bot so that admins-only commands work.'
+        response += '\nPlease use the command `/update_admin_roles` to set admin roles for this bot so that admins-only commands work.'
         await interaction.response.send_message(response, ephemeral=True)
         return
     # Check if the user has one of the admin roles set for the bot:
@@ -108,7 +117,7 @@ async def toggle_one_number_per_user(interaction):
 
     counting.CountingData.toggle_one_number_per_user()
     response = "ONE_NUMBER_PER_USER has been turned "
-    response += "ON" if counting.CountingData.one_number_per_user else "OFF"
+    response += "**ON**" if counting.CountingData.one_number_per_user else "**OFF**"
     response += "."
     await interaction.response.send_message(response)
 
@@ -118,7 +127,7 @@ async def toggle_one_number_per_user(interaction):
     guild=MY_GUILD
 )
 async def get_counting_lives(interaction):
-    response = f"You now have {counting.CountingData.current_lives} lives remaining."
+    response = f"You now have **{counting.CountingData.current_lives}** lives remaining."
     await interaction.response.send_message(response)
 
 @tree.command(
@@ -127,12 +136,12 @@ async def get_counting_lives(interaction):
     guild=MY_GUILD
 )
 async def get_counting_info(interaction):
-    response = f"CURRENT LIVES: {counting.CountingData.current_lives}\n"
-    response += f"CURRENT INTEGER: {counting.CountingData.current_int}\n"
-    response += f"MAX LIVES: {counting.CountingData.max_lives}\n"
-    response += f"COUNTING HIGH SCORE: {counting.CountingData.max_int}\n"
-    response += "ONE NUMBER PER USER: "
-    response += "On" if counting.CountingData.one_number_per_user else "Off"
+    response = f"Current number of lives: **{counting.CountingData.current_lives}**\n"
+    response += f"Current integer: **{counting.CountingData.current_int}**\n"
+    response += f"Max lives: **{counting.CountingData.max_lives}**\n"
+    response += f"Counting high score: **{counting.CountingData.max_int}**\n"
+    response += "ONE_NUMBER_PER_USER: "
+    response += "**ON**" if counting.CountingData.one_number_per_user else "**OFF**"
     await interaction.response.send_message(response)
 
 @tree.command(
@@ -141,7 +150,7 @@ async def get_counting_info(interaction):
     guild=MY_GUILD
 )
 async def counting_high_score(interaction):
-    response = f"COUNTING HIGH SCORE: {counting.CountingData.max_int}"
+    response = f"Counting high score: **{counting.CountingData.max_int}**"
     await interaction.response.send_message(response)
 
 @tree.command(
@@ -155,7 +164,7 @@ async def set_max_lives_to(interaction, new_max_lives: str):
     roles_are_set = await admin_roles_are_set()
     if not roles_are_set:
         response = 'Admin roles for this bot are not set yet.'
-        response += '\nPlease use the command /update_admin_roles to set admin roles for this bot so that admins-only commands work.'
+        response += '\nPlease use the command `/update_admin_roles` to set admin roles for this bot so that admins-only commands work.'
         await interaction.response.send_message(response, ephemeral=True)
         return
     # Check if the user has one of the admin roles set for the bot:
@@ -176,7 +185,7 @@ async def set_max_lives_to(interaction, new_max_lives: str):
         new_max = int(new_max_lives)
         if new_max <= 0: # Terminate if new_max is not positive
             await interaction.response.send_message(
-                f'Argument {new_max_lives} is not above 0.\nThis command has not been fulfilled.'
+                f'Argument **{new_max_lives}** is not above 0.\nThis command has not been fulfilled.'
             )
             return
 
@@ -185,17 +194,17 @@ async def set_max_lives_to(interaction, new_max_lives: str):
             counting.CountingData.set_max_lives_and_reset(new_max)
         else: # Simply set max_lives to new_max
             counting.CountingData.set_max_lives(new_max)
-        response = f"MAX LIVES has been set to {new_max}"
+        response = f"MAX LIVES has been set to **{new_max}**"
         await interaction.response.send_message(response)
     except ValueError:
         await interaction.response.send_message(
-            f'Argument {new_max_lives} cannot be parsed.\nThis command has not been fulfilled.'
+            f'Argument **{new_max_lives}** cannot be parsed.\nThis command has not been fulfilled.'
         )
 
 
 @tree.command(
     name="get_admin_roles",
-    description="Get a list of the admin roles for this server",
+    description="Get a list of the admin roles for this server (Admins-only)",
     guild=MY_GUILD
 )
 async def get_admin_roles(interaction):
@@ -203,14 +212,27 @@ async def get_admin_roles(interaction):
     roles_are_set = await admin_roles_are_set()
     if not roles_are_set:
         response = 'Admin roles for this bot are not set yet.'
-        response += '\nPlease use the command /update_admin_roles to set admin roles for this bot so that admins-only commands work.'
+        response += '\nPlease use the command `/update_admin_roles` to set admin roles for this bot so that admins-only commands work.'
         await interaction.response.send_message(response, ephemeral=True)
+        return
+    # Check if the user has one of the admin roles set for the bot:
+    member_is_admin = False
+    for role in interaction.user.roles:
+        if role in admin.AdminData.admin_roles:
+            member_is_admin = True
+            break
+    if not member_is_admin:
+        # Send an ephemeral message to the user
+        await interaction.response.send_message(
+            'This command cannot be carried out because you do not have one of the admin roles set for this bot.',
+            ephemeral=True
+        )
         return
 
     admin_roles = admin.AdminData.admin_roles
     admin_roles_names: List[str] = [role.name for role in admin_roles]
 
-    response = f"Admin roles for this bot: {admin_roles_names}"
+    response = f"Admin roles for this bot: *{admin_roles_names}*"
     await interaction.response.send_message(response)
 
 @tree.command(
@@ -252,7 +274,7 @@ async def update_admin_roles(interaction, admins: str):
         admin_roles = admin.AdminData.admin_roles
         admin_roles_names: List[str] = [role.name for role in admin_roles]
 
-        response += f"\nUpdated admin roles: {admin_roles_names}"
+        response += f"\nUpdated admin roles: *{admin_roles_names}*"
         await interaction.response.send_message(response)
 
 @tree.command(
@@ -279,12 +301,39 @@ async def reset_admin_roles(interaction):
             return
     else:
         response = 'Admin roles for this bot are not set yet.'
-        response += '\nPlease use the command /update_admin_roles to set admin roles for this bot so that admins-only commands work.'
+        response += '\nPlease use the command `/update_admin_roles` to set admin roles for this bot so that admins-only commands work.'
         await interaction.response.send_message(response, ephemeral=True)
 
     admin.AdminData.admin_roles.clear()
     await interaction.response.send_message('Admin roles for this bot have been cleared.')
 
+@tree.command(
+    name="help",
+    description="Get available commands from this bot",
+    guild=MY_GUILD
+)
+async def bot_help(interaction):
+    response = '''## General:
+    `/help`: Get available commands from this bot
+    `/hey`: Say hey to the bot!
+    `/hello`: Say hello to the bot!
+    
+    ## Counting:
+    `/counting_mode`: Check the status of the counting mode (ON/OFF)
+    `/toggle_counting`: Toggle the status of the counting mode (ON/OFF) (Admins-only)
+    `/one_number_per_user`: Check the status of the one_number_per_user setting for counting (ON/OFF)
+    `/toggle_one_number_per_user`: Toggle the status of the one_number_per_user setting for counting (ON/OFF) (Admins-only)
+    `/counting_lives`: Check the number of lives remaining before the counting game is reset
+    `/counting_info`: Return all information about the counting game
+    `/counting_high_score`: Check the high score achieved in the counting game
+    `/set_max_lives_to <positive-integer>` : Set the number of max counting lives to a certain positive integer (Admins-only)
+    
+    ## Administrative:
+    `/get_admin_roles`: Get a list of the admin roles for this server (Admins-only)
+    `/update_admin_roles`: Update admin roles for this bot with names (Admins-only if admin roles for this bot have been set)
+    `/reset_admin_roles`: Reset admin roles for this bot (Admins-only if admin roles for this bot have been set)
+    '''
+    await interaction.response.send_message(response, ephemeral = True)
 
 @client.event
 async def on_message(message: discord.Message):
@@ -297,7 +346,7 @@ async def on_message(message: discord.Message):
 def toggle_counting():
     counting.CountingData.counting_mode = not counting.CountingData.counting_mode
     response = "Counting mode has been turned "
-    response += "ON" if counting.CountingData.counting_mode else "OFF"
+    response += "**ON**" if counting.CountingData.counting_mode else "**OFF**"
     response += "."
     return response
 
